ecb26092b03cd140dd9c891ce3ae8de12a57280b
Byk0X <maciej.lencewicz@gmail.com>
2025-05-15 01:11:48 +0200

    cleaning code

--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -15,22 +15,19 @@ class WeatherViewModel : ViewModel() {
 
     var isLoading by mutableStateOf(false)
 
-    // Metoda fetchWeather, która wykonuje zapytanie do API
     fun fetchWeather(city: String, apiKey: String, onResult: (WeatherResponse?) -> Unit) {
-        // Ustawiamy loading na true podczas pobierania danych
         isLoading = true
 
-        // Uruchamiamy korutynę w viewModelScope
         viewModelScope.launch {
             try {
                 val response = RetrofitClient.apiService.getWeather(city, apiKey)
                 weatherResponse = response
-                onResult(response)  // Zwracamy dane do Composables
+                onResult(response)
             } catch (e: Exception) {
                 Log.e("WeatherVM", "Błąd przy pobieraniu pogody: ${e.message}")
                 onResult(null)
             } finally {
-                isLoading = false  // Ustawiamy loading na false, po zakończeniu
+                isLoading = false
             }
         }
     }
