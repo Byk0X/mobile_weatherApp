64c38e59c93d8b5d22323a482005e08cde5bf2e4
Byk0X <maciej.lencewicz@gmail.com>
2025-05-15 21:09:17 +0200

    added third option for unit

--- app/src/main/java/com/example/weatherapp/EnumUnits.kt
+++ app/src/main/java/com/example/weatherapp/EnumUnits.kt
@@ -1,8 +1,8 @@
 package com.example.weatherapp
 
-enum class UnitSystem(val apiValue: String, val label: String) {
-    Standard("standard", "Standard (K, m/s)"),
-    Metric("metric", "Metrical (°C, km/h)"),
-    Imperial("imperial", "Imperial (°F, mph)")
-}
+enum class UnitSystem(val apiValue: String, val label: String, val tempLabel: String, val speedLabel: String) {
 
+    Metric("metric", "Metryczne (°C, km/h)", "°C", "km/h"),
+    Imperial("imperial", "Imperialne (°F, mph)", "°F", "mph"),
+    Standard("standard", "Standardowe (K, m/s)", "K", "m/s")
+}
