6fa2dda03cc7340b33407b20e462475d1b226da9
Byk0X <maciej.lencewicz@gmail.com>
2025-05-15 03:12:19 +0200

    enum units

--- app/src/main/java/com/example/weatherapp/EnumUnits.kt
+++ app/src/main/java/com/example/weatherapp/EnumUnits.kt
@@ -1,4 +1,8 @@
 package com.example.weatherapp
 
-class EnumUnits {
+enum class UnitSystem(val apiValue: String, val label: String) {
+    Standard("standard", "Standard (K, m/s)"),
+    Metric("metric", "Metrical (°C, km/h)"),
+    Imperial("imperial", "Imperial (°F, mph)")
 }
+
