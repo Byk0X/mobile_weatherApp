d6162156cd9054e348c4d6a2d3b51ba4f5712f17
Byk0X <maciej.lencewicz@gmail.com>
2025-05-16 21:42:55 +0200

    util used for topbar

--- app/src/main/java/com/example/weatherapp/utils.kt
+++ app/src/main/java/com/example/weatherapp/utils.kt
@@ -1,2 +1,8 @@
 package com.example.weatherapp
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class TabItem(val label: String) {
+    class TextTab(label: String) : TabItem(label)
+    class IconTab(val icon: ImageVector, label: String) : TabItem(label)
+}
