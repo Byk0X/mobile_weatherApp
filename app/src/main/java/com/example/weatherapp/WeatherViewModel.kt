cc127d09180f8082478aff42b348d4872b250174
Byk0X <maciej.lencewicz@gmail.com>
2025-05-16 21:42:34 +0200

    restored

--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp
 
+import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -8,14 +9,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.State
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import androidx.core.content.edit
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class WeatherViewModel : ViewModel() {
 
-    var weatherResponse by mutableStateOf<WeatherResponse?>(null)
-        private set
 
-    var forecastResponse by mutableStateOf<ForecastResponse?>(null)
-        private set
+    private val _weatherResponse = MutableStateFlow<WeatherResponse?>(null)
+    val weatherResponse: StateFlow<WeatherResponse?> = _weatherResponse
+
+    private val _forecastResponse = MutableStateFlow<ForecastResponse?>(null)
+    val forecastResponse: StateFlow<ForecastResponse?> = _forecastResponse
 
     var isLoading by mutableStateOf(false)
 
@@ -33,13 +40,91 @@ class WeatherViewModel : ViewModel() {
         _currentCity.value = newCity
     }
 
+    private val _lastWeatherData = mutableStateOf<WeatherResponse?>(null)
+    val lastWeatherData: State<WeatherResponse?> = _lastWeatherData
+
+    private val _lastForecastData = mutableStateOf<ForecastResponse?>(null)
+    val lastForecastData: State<ForecastResponse?> = _lastForecastData
+
+
+    fun saveWeatherToPreferences(context: Context, data: WeatherResponse) {
+        try {
+            val prefs = context.getSharedPreferences("weather_prefs", Context.MODE_PRIVATE)
+            prefs.edit {
+
+                val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                val adapter = moshi.adapter(WeatherResponse::class.java)
+                val json = adapter.toJson(data)
+
+                putString("last_weather", json)
+            }
+
+            Log.d("WeatherVM", "Weather saved to SharedPreferences.")
+        } catch (e: Exception) {
+            Log.e("WeatherVM", "Error saving weather to preferences", e)
+        }
+    }
+
+    fun loadWeatherFromPreferences(context: Context): WeatherResponse? {
+        return try {
+            val prefs = context.getSharedPreferences("weather_prefs", Context.MODE_PRIVATE)
+            val json = prefs.getString("last_weather", null)
+            Log.d("prefs", "Raw weather: $json")
+            if (json.isNullOrEmpty()) return null
+
+            val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+            val adapter = moshi.adapter(WeatherResponse::class.java)
+            adapter.fromJson(json)
+
+        } catch (e: Exception) {
+            Log.e("WeatherVM", "Error loading weather from preferences", e)
+            null
+        }
+    }
+
+    fun saveForecastToPreferences(context: Context, data: ForecastResponse) {
+        try {
+            val prefs = context.getSharedPreferences("forecast_prefs", Context.MODE_PRIVATE)
+            prefs.edit {
+
+                val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                val adapter = moshi.adapter(ForecastResponse::class.java)
+                val json = adapter.toJson(data)
+
+                putString("last_forecast", json)
+            }
+
+            Log.d("WeatherVM", "forecast saved to SharedPreferences.")
+        } catch (e: Exception) {
+            Log.e("WeatherVM", "Error saving forecast to preferences", e)
+        }
+    }
+
+    fun saveLastWeatherData(context: Context, data: WeatherResponse) {
+        _lastWeatherData.value = data
+        saveWeatherToPreferences(context, data)
+    }
+
+    fun loadLastWeatherData(context: Context) {
+        val weather = loadWeatherFromPreferences(context)
+        if (weather != null) {
+            _lastWeatherData.value = weather
+            _weatherResponse.value = weather
+        }
+    }
+
+    fun saveLastForecastData(context: Context, data: ForecastResponse) {
+        _lastForecastData.value = data
+        saveForecastToPreferences(context, data)
+    }
+
     fun fetchWeather(city: String, units: String, apiKey: String, lang: String, onResult: (WeatherResponse?) -> Unit) {
         isLoading = true
 
         viewModelScope.launch {
             try {
                 val response = RetrofitClient.apiService.getWeather(city, units ,apiKey, lang)
-                weatherResponse = response
+                _weatherResponse.value = response
                 onResult(response)
             } catch (e: Exception) {
                 Log.e("WeatherVM", "Błąd przy pobieraniu pogody: ${e.message}")
@@ -56,11 +141,11 @@ class WeatherViewModel : ViewModel() {
         viewModelScope.launch{
             try {
                 val response = RetrofitClient.forecastApiService.getForecast(city, units, apiKey, lang)
-                forecastResponse = response
+                _forecastResponse.value = response
                 onResult(response)
             } catch (e: Exception) {
-            Log.e("WeatherVM", "Błąd przy pobieraniu prognozy: ${e.message}")
-            onResult(null)
+                Log.e("WeatherVM", "Błąd przy pobieraniu prognozy: ${e.message}")
+                onResult(null)
             } finally {
                 isLoading = false
             }
