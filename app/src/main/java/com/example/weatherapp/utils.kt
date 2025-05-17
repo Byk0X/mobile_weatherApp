b0cae228ecad267f804cd1432a64a27e2ed531ae
Byk0X <maciej.lencewicz@gmail.com>
2025-05-17 21:21:13 +0200

    Add: Function to normalize special characters in city names

--- app/src/main/java/com/example/weatherapp/utils.kt
+++ app/src/main/java/com/example/weatherapp/utils.kt
@@ -6,3 +6,17 @@ sealed class TabItem(val label: String) {
     class TextTab(label: String) : TabItem(label)
     class IconTab(val icon: ImageVector, label: String) : TabItem(label)
 }
+
+//changes special letters to latin
+fun cityNameWithoutSpecialLetters(cityName: String): String {
+
+    val map = mapOf(
+        'ą' to 'a', 'ć' to 'c', 'ę' to 'e', 'ł' to 'l', 'ń' to 'n',
+        'ó' to 'o', 'ś' to 's', 'ż' to 'z', 'ź' to 'z',
+        'Ą' to 'A', 'Ć' to 'C', 'Ę' to 'E', 'Ł' to 'L', 'Ń' to 'N',
+        'Ó' to 'O', 'Ś' to 'S', 'Ż' to 'Z', 'Ź' to 'Z'
+    )
+
+    return cityName.map { letter -> map[letter] ?: letter }.joinToString("")
+
+}
